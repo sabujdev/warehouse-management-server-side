07d0aaee6877e73de3a22998255b0d661bba8bd7
sabujmahonto <sabujmahonto@gmail.com>
2022-05-03 20:25:26 +0600

    delete api added

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient,ServerApiVersion,ObjectId} = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -14,52 +13,58 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pung5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-        await client.connect();
-        const serviceCollection = client.db('AllItem').collection('product');
-        // get request 
-        // http://localhost:5000/products
+  try {
+    await client.connect();
+    const serviceCollection = client.db("AllItem").collection("product");
+    // get request
+    // http://localhost:5000/products
 
-        app.get('/products', async (req, res) => {
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
-        });
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-        app.get('/product/:id', async(req, res) =>{
-            const id = req.params.id;
-            const query={_id: ObjectId(id)};
-            const service = await serviceCollection.findOne(query);
-            res.send(service);
-        });
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
 
-        // POST
-        app.post('/service', async(req, res) =>{
-            const newService = req.body;
-            const result = await serviceCollection.insertOne(newService);
-            res.send(result);
-        });
-       
+    // POST
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
 
-       
-
-    }
-    finally {
-        // await client.close();
-    }
+    // DELETE
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
 }
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Running Genius Server');
+app.get("/", (req, res) => {
+  res.send("Running Genius Server");
 });
 
 app.listen(port, () => {
-    console.log('Listening to port', port);
-})
+  console.log("Listening to port", port);
+});
