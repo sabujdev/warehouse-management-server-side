8b5406f7dcc5537594b3fddbe752707045066d4d
sabujmahonto <sabujmahonto@gmail.com>
2022-05-03 20:21:59 +0600

    get api creating done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient,ServerApiVersion,ObjectId} = require('mongodb');
+
+
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +11,47 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pung5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('AllItem').collection('product');
+        // get request 
+        // http://localhost:5000/products
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/product/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+       
+
+       
+       
+
+       
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running Genius Server');
